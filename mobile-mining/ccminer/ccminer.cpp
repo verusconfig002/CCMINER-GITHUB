9675216de4981b116eab1aee419dcb346004b514
saengx <157899625+saengx@users.noreply.github.com>
2024-04-16 22:17:51 +0700

    Update ccminer.cpp

--- mobile-mining/ccminer/ccminer.cpp
+++ mobile-mining/ccminer/ccminer.cpp
@@ -884,7 +884,9 @@ int share_result(int result, int pooln, double sharediff, const char *reason)
 		sprintf(solved, " solved: %u", p->solved_count);
 	}
 
-	applog(LOG_NOTICE, "\033[1;37;45m INFO   \033[0m Accepted: \033[36m%lu/%lu\033[0m (\033[33m%s\033[0m), \033[36m%s %s%s\033[0m",
+	applog(LOG_NOTICE, "
+\033[1;37;45m INFO   \033[0m Accepted: \033[36m%lu\033[0m 
+Rejected: \033[36m%lu\033[0m, \033[36m%s %s%s\033[0m",
 			p->accepted_count,
 			p->accepted_count + p->rejected_count,
 			suppl, s, flag, solved);
